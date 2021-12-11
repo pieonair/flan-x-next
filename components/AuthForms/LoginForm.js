dbb411f59e3ec0cca914a80ed2b3db725a7d5fa5
bendevski <fb1112@nyu.edu>
2021-12-11 15:32:47 +0000

    why are things broken

--- components/AuthForms/LoginForm.js
+++ components/AuthForms/LoginForm.js
@@ -1,15 +1,22 @@
 import {useForm} from "react-hook-form";
 import axios from "axios";
 import Header from '/components/Header';
+import { setCookies, getCookie } from 'cookies-next';
 
 const LoginForm = () => {
     const {
         register,
-        handleSubmit
+        handleSubmit,
+        setValue
     } = useForm();
+    
+    setValue("user", getCookie("userName"));
     const onSubmit = async (data) => {
         try{
             const {user, password} = data;
+            if (data.checkbox) setCookies("userName", user);
+            else setCookies("userName", null);
+            
             let response = await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL+ "/login", {username:user, password:password});
             const jwt = response.data.token;
             sessionStorage.setItem('tok', jwt);
@@ -35,6 +42,7 @@ const LoginForm = () => {
                     <button className="text-button-blue">Forgot your password?</button>
                     </div>
                     <div className="flow-root">
+                    <input type="checkbox" {...register("save")}/>
                     <button className="text-button-blue">Create Account</button>
                     </div>
                 </form> 
@@ -45,3 +53,4 @@ const LoginForm = () => {
 }
 
 export default LoginForm;
+
