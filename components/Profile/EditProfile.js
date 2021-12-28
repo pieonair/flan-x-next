72dc715a992a6331d4dd3cca7cd2d728ed8a7428
bendevski <fb1112@nyu.edu>
2021-12-28 09:28:07 +0000

    fix edit profile errors | make pretty

--- components/Profile/EditProfile.js
+++ components/Profile/EditProfile.js
@@ -1,7 +1,7 @@
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/router";
-import Popup from 'reactjs-popup';
+import Popup from "reactjs-popup";
 
 const fields = {
     username: { type: String, required: 1 },
@@ -23,73 +23,104 @@ const fields = {
     skills: { type: [String] },
     services: { type: [Service] },
     career: { type: [Experience] },
-    savedList: { type: [UserReference], default: [] } ,
+    savedList: { type: [UserReference], default: [] },
     private: { type: Boolean, default: false },
-    mailing: { type: Boolean },
-}
+    mailing: { type: Boolean }
+};
 
 const EditProfileForm = () => {
     const {
         register,
         handleSubmit,
         setError,
-        formState: {errors},
+        formState: { errors },
         clearErrors
     } = useForm();
-    const router = useRouter()
+    const router = useRouter();
     useEffect(() => {
-        () =>{
-            if (!sessionStorage.getItem('tok'))
-                router.replace('/');
+        async () => {
+            if (!sessionStorage.getItem("tok")) router.replace("/");
             //send request to get current details
             const data = await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL);
-        }},[]);
+        };
+    }, []);
 
-        const onSubmit = async (data) => {
-        try{
+    const onSubmit = async (data) => {
+        try {
             clearErrors();
-            const {username, name,  email, type, about, personalInfo, freelancerSites, skills, services, career, savedList, private, mailing} = data;
-            const tok = sessionStorage.getItem('tok'); // add this to the axios header
-            let response = await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL+ "/edit", 
-            {tok:tok, username:username, name:name, email:email, type:type, about:about, personalInfo:personalInfo, freelancerSites:freelancerSites, skills:skills, services:services, career:career, savedList:savedList, private:private, mailing:mailing},
-            {headers: {'Authorization': tok}});
-        }
-        catch(err){
+            const {
+                username,
+                name,
+                email,
+                type,
+                about,
+                personalInfo,
+                freelancerSites,
+                skills,
+                services,
+                career,
+                savedList,
+                is_private,
+                mailing
+            } = data;
+            const tok = sessionStorage.getItem("tok"); // add this to the axios header
+            let response = await axios.post(
+                process.env.NEXT_PUBLIC_BACKEND_URL + "/edit",
+                {
+                    tok: tok,
+                    username: username,
+                    name: name,
+                    email: email,
+                    type: type,
+                    about: about,
+                    personalInfo: personalInfo,
+                    freelancerSites: freelancerSites,
+                    skills: skills,
+                    services: services,
+                    career: career,
+                    savedList: savedList,
+                    is_private: is_private,
+                    mailing: mailing
+                },
+                { headers: { Authorization: tok } }
+            );
+        } catch (err) {
             console.log(err);
         }
-    }
+    };
 
-    return ( // is error checking for valid username/password already being done automatically below? 
+    return (
+        // is error checking for valid username/password already being done automatically below?
         <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input type = "text" {...register("username"), defaultValue = {username}}/> 
-            {errors.username && errors.username.message}
-            <input type = "text" {...register("email"), defaultValue = {email}} />
-            {errors.email && errors.email.message}
-            <input {...register("type"), defaultValue = {type}} /> 
-            {errors.type && errors.username.type}
-            <input type = "text" {...register("about"), defaultValue = {about}} />
-            {errors.about && errors.username.about}
-            <input {...register("personalInfo"), defaultValue = {personalInfo}} />
-            {errors.personalInfo && errors.personalInfo.message}
-            <input {...register("freelancerSites"), defaultValue = {freelancerSites}} />
-            {errors.freelancerSites && errors.freelancerSites.message}
-            <input type = "text"  {...register("skills"), defaultValue = {skills}} />
-            {errors.skills && errors.skills.message}
-            <input {...register("services"), defaultValue = {services}} />
-            {errors.services && errors.services.message}
-            <input {...register("career"), defaultValue = {career}} />
-            {errors.career && errors.career.message}
-            <input {...register("savedList"), defaultValue = {savedList}} />
-            {errors.savedList && errors.savedList.message}
-            <input {...register("private"), defaultValue = {private}} />
-            {errors.private && errors.private.message}
-            <input {...register("mailing"), defaultValue = {mailing}} />
-            {errors.mailing && errors.mailing.message}
-            <input type="submit" value="editProfile" />
-          </form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" {...register("username")} />
+                {errors.username && errors.username.message}
+                <input type="text" {...register("email")} />
+                {errors.email && errors.email.message}
+                <input {...register("type")} />
+                {errors.type && errors.username.type}
+                <input type="text" {...register("about")} />
+                {errors.about && errors.username.about}
+                <input {...register("personalInfo")} />
+                {errors.personalInfo && errors.personalInfo.message}
+                <input {...register("freelancerSites")} />
+                {errors.freelancerSites && errors.freelancerSites.message}
+                <input type="text" {...register("skills")} />
+                {errors.skills && errors.skills.message}
+                <input {...register("services")} />
+                {errors.services && errors.services.message}
+                <input {...register("career")} />
+                {errors.career && errors.career.message}
+                <input {...register("savedList")} />
+                {errors.savedList && errors.savedList.message}
+                <input {...register("is_private")} />
+                {errors.private && errors.private.message}
+                <input {...register("mailing")} />
+                {errors.mailing && errors.mailing.message}
+                <input type="submit" value="editProfile" />
+            </form>
         </div>
-      );
-}
+    );
+};
 
 export default EditProfileForm;
